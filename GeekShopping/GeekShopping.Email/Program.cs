0b8d6ac5be85254df2a8fc15c42680a5f2ca544e
Henrique Teixeira <henriquecesarth22@gmail.com>
2025-03-18 15:38:06 -0300

    Implementação de um serviço de Email Mock

--- GeekShopping/GeekShopping.Email/Program.cs
+++ GeekShopping/GeekShopping.Email/Program.cs
@@ -1,23 +1,98 @@
+
+using System.Collections.Generic;
+using GeekShopping.Email.MessageConsumer;
+using GeekShopping.Email.Model.Context;
+using GeekShopping.Email.Repository;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Configuração do banco de dados
+var connection = builder.Configuration["MySQlConnection:MySQlConnectionString"];
+builder.Services.AddDbContext<MySQLContext>(options => options.
+    UseMySql(connection, ServerVersion.AutoDetect(connection)));
 
+var mySqlBuilder = new DbContextOptionsBuilder<MySQLContext>();
+mySqlBuilder.UseMySql(connection, ServerVersion.AutoDetect(connection));
+
+// Configuração do repositório
+builder.Services.AddSingleton(new EmailRepository(mySqlBuilder.Options));
+builder.Services.AddScoped<IEmailRepository, EmailRepository>();
+
+// Configuração do Rabbit
+builder.Services.AddHostedService<RabbitMQPaymentConsumer>();
+// builder.Services.AddSingleton<IRabbitMQMessageSender, RabbitMQMessageSender>();
+
+
+// Adicionando serviços ao contêiner
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddAuthentication("Bearer")
+    .AddJwtBearer("Bearer", options =>
+    {
+        options.Authority = "https://localhost:4435/";
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateAudience = false
+        };
+    });
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("ApiScope", policy =>
+    {
+        policy.RequireAuthenticatedUser();
+        policy.RequireClaim("scope", "geek_shopping");
+    });
+});
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "GeekShopping.Email", Version = "v1" });
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = "Enter 'Bearer' [space] and your token!",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                },
+                Scheme = "oauth2",
+                Name = "Bearer",
+                In= ParameterLocation.Header
+            },
+            new List<string> ()
+        }
+    });
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configuração do pipeline de requisições HTTP
 if (app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage();
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "GeekShopping.Email v1"));
 }
 
 app.UseHttpsRedirection();
-
+app.UseRouting();
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
---
+++ GeekShopping/GeekShopping.Email/Repository/IEmailRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using GeekShopping.Email.Messages;
+
+namespace GeekShopping.Email.Repository
+{
+    public interface IEmailRepository
+    {
+        Task LogEmail(UpdatePaymentResultMessage message);
+    }
+}
