c7761037dc612b895a1bb5574c38f2ec16a0bf0e
Henrique Teixeira <henriquecesarth22@gmail.com>
2025-03-20 16:03:51 -0300

    Implementação do Ocelot

---
+++ GeekShopping/GeekShopping.APIGateway/Program.cs
@@ -0,0 +1,40 @@
+using Microsoft.IdentityModel.Tokens;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddAuthentication("Bearer")
+    .AddJwtBearer("Bearer", options =>
+    {
+        options.Authority = "https://localhost:4435/";
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateAudience = false
+        };
+    });
+
+builder.Services.AddOcelot();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthentication(); // 🔹 Adicionei autenticação antes do Ocelot
+app.UseAuthorization();  // 🔹 Adicionei autorização antes do Ocelot
+
+app.UseOcelot().Wait(); // 🔹 Chamar corretamente com `.Wait()`
+
+app.Run();
